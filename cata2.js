0e655369652ecb3e154f95f326688f759de5c449
Jose Antonio Grados Barriga <gradosj@gmail.com>
2019-12-15 09:41:13 +0100

    Realizada la funcion de sustituir valores

--- cata2.js
+++ cata2.js
@@ -1,6 +1,7 @@
 let nRomanos = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
 let nValor = [1, 5, 10, 50, 100, 500, 1000];
-let miNumR = 'XXXX';
+let miNumR = 'XXX', miNumA;
+let myValues = [];
 let contI = 0, contX = 0, contC = 0, contM = 0, contV = 0,
 	contL = 0, contD = 0;
 let infoError = [false,' '];
@@ -45,52 +46,51 @@ const valida_periodicidad = (miNumR) => {
 		return [true,'E002'];
 		
 	}
+
+	return [false, ' '];
 		
 		
 }
 
 
-const validadedValor = (miNumR) => {
-
+const convertValues = (miNumR) => {
+    let x = 0;
+	let valueFounded = false;
+	let myValuesAux = [];
+	
 	for (let n = 0; n < miNumR.length; n++) {
-		switch (miNumR[n]) {
-			case 'I':
-				contI ++;
-				break;
-			case 'X':
-				contX ++;
-				break;
-			case 'C':
-				contC ++;
-				break;
-			case 'M':
-				contM ++;
-				break;
-			case 'V':
-				contV ++;
-				break;
-			case 'L':
-				contL ++;
-				break;
-			case 'D':
-				contD ++;
-				break;
+		while (valueFounded == false){
+			if (miNumR[n] == nRomanos[x]){
+				myValuesAux[n] = nValor[x]; 
+				valueFounded = true; 
+				x++;
+			}
+			x++;
 			
 		}
+		valueFounded = false;
+		x = 0; 
 
-	}
+		}
+	
+		return myValuesAux;
 
-	if (contI > 3 || contX > 3 || contC > 3 || contM > 3){
-		return [true,'E001'];
-		
-	}
 
-	if (contV > 1 || contL > 1 || contD > 1){
-		return [true,'E002'];
 		
-	}
-		
+	
 }
-infoError = valida_periodicidad(miNumR); 
 
+		
+
+infoError = valida_periodicidad(miNumR); 
 console.log(infoError);
+
+if (infoError[0] == false){
+	myValues = convertValues(miNumR);
+	console.log(myValues);
+
+}
+
+
+
+
