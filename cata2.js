d641859df8f8410ad4035efca07ab2d9eeb7cc62
Jose Antonio Grados Barriga <gradosj@gmail.com>
2019-12-15 11:15:01 +0100

    Primera pruebas del conversor de romanos a arabes OK

--- cata2.js
+++ cata2.js
@@ -1,6 +1,6 @@
 let nRomanos = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
 let nValor = [1, 5, 10, 50, 100, 500, 1000];
-let miNumR = 'XXX', miNumA;
+let miNumR = 'LV', miNumA;
 let myValues = [];
 let contI = 0, contX = 0, contC = 0, contM = 0, contV = 0,
 	contL = 0, contD = 0;
@@ -24,13 +24,29 @@ const valida_periodicidad = (miNumR) => {
 				contM ++;
 				break;
 			case 'V':
+				if (miNumR[n + 1] ==  'X' || 
+					miNumR[n + 1] ==  'L' ||
+					miNumR[n + 1] ==  'C' ||
+					miNumR[n + 1] ==  'D' ||
+					miNumR[n + 1] ==  'M' ){
+						return [true,'Incorrecto: Los símbolos V, L y D no pueden colocarse a la izquierda de otro mayor.'];
+					}
+
 				contV ++;
 				break;
 			case 'L':
 				contL ++;
+				if (miNumR[n + 1] ==  'C' || 
+					miNumR[n + 1] ==  'D' ||
+					miNumR[n + 1] ==  'M' ){
+						return [true,'Incorrecto: Los símbolos V, L y D no pueden colocarse a la izquierda de otro mayor.'];
+					}
 				break;
 			case 'D':
 				contD ++;
+				if (miNumR[n + 1] ==  'M' ){
+						return [true,'Incorrecto: Los símbolos V, L y D no pueden colocarse a la izquierda de otro mayor.'];
+				}
 				break;
 			
 		}
@@ -38,12 +54,12 @@ const valida_periodicidad = (miNumR) => {
 	}
 
 	if (contI > 3 || contX > 3 || contC > 3 || contM > 3){
-		return [true,'E001'];
+		return [true,'Incorrecto: Los símbolos I, X, C y M se pueden repetir hasta tres veces'];
 		
 	}
 
 	if (contV > 1 || contL > 1 || contD > 1){
-		return [true,'E002'];
+		return [true,'Incorrecto: Los símbolos V, L y D no pueden repetirse'];
 		
 	}
 
@@ -80,6 +96,18 @@ const convertValues = (miNumR) => {
 	
 }
 
+const operar = (myValues) => {
+	for (let n = 0; n < myValues.length; n++) {
+		if (myValues[n] < myValues[n + 1]){
+			myValues[n] = myValues[n] * -1;
+		} 
+
+		}
+		console.log(myValues)
+		return myValues;
+
+}
+
 		
 
 infoError = valida_periodicidad(miNumR); 
@@ -88,6 +116,9 @@ console.log(infoError);
 if (infoError[0] == false){
 	myValues = convertValues(miNumR);
 	console.log(myValues);
+	myValues = operar(myValues);
+	miNumA = myValues.reduce((a, b) => a + b, 0);
+	console.log(miNumA);
 
 }
 
